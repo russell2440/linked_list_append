759e5bc2ba30a51f244d64d6fa7cd67c778d40f9
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-07-05 22:43:03 +0000

    Updated to use node class

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-// Add append method to linked list class
+// Node class for linked list node creation
+class node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+
+// Create linked list with prepend and
+// append methods, using node class.
 class linked_list{
   constructor(value){
-    this.head = {
-      value: value,
-      next: null
-    };
+    let new_node = new node(value);
+    this.head = new_node;
     this.tail = this.head;
     this.length = 1;
   };
   append(value){
-    const new_node = {
-      value: value,
-      next: null
-    };
     // This node will be the new tail.
     // Point the old tail to this node, 
     // point the tail to this node.
+    let new_node = new node(value);
     this.tail.next = new_node;
     this.tail = new_node;
     this.length++;
     return this;
   };
   prepend(value){
-    const new_node = {
-      value: value,
-      next: null
-    };
+    let new_node = new node(value);
     new_node.next = this.head;
     this.head = new_node;
     this.length++;
