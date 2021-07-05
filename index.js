6639d6d47cf56377f4f8773faa3dc266704b704f
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-07-05 22:21:13 +0000

    filled in append and prepend

--- index.js
+++ index.js
@@ -9,10 +9,26 @@ class linked_list{
     this.length = 1;
   };
   append(value){
-
+    const new_node = {
+      value: value,
+      next: null
+    };
+    // This node will be the new tail.
+    // Point the tail's next to this node, point the tail to this node.
+    this.tail.next = new_node;
+    this.tail = new_node;
+    this.length++;
+    return this;
   };
   prepend(value){
-    
+    const new_node = {
+      value: value,
+      next: null
+    };
+    // This node will be the new tail.
+    new_node.next = this.head;
+    this.head = new_node;
+    this.length++;
   }
 };
 
